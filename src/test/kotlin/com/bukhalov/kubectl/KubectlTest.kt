5f2d7bc67dd9b7611c8d7d698837aba98ca899d0
Aleksei Bukhalov <buhaloff.aleks@gmail.com>
2023-11-03 10:54:53 +0100

    debug logs

--- src/test/kotlin/com/bukhalov/kubectl/KubectlTest.kt
+++ src/test/kotlin/com/bukhalov/kubectl/KubectlTest.kt
@@ -1,6 +1,7 @@
 package com.bukhalov.kubectl
 
 import com.bukhalov.kubectl.utils.executeCommand
+import com.bukhalov.kubectl.utils.logger
 import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
@@ -9,7 +10,8 @@ class KubectlTest : FeatureSpec({
     feature("version info") {
         scenario("should allow user to check kubectl version") {
             val result = executeCommand("kubectl version")
-
+            logger.info { "Result:" + result.output }
+            logger.info { "Result.error:" + result.error }
 
             result.exitCode shouldBe 0
             result.output shouldContain "Client Version"
